5639a89433832d1a8226224049360d4470fc3029
Lucas Colantuono <colantuonolucas@gmail.com>
2019-10-10 15:01:05 -0300

    integracion, validacion browser

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
+require './lib/PregResp.rb'
 
 get '/' do
+  @@Preg_resp = PregResp.new
+  @@Preg_resp.set_preg_resp "¿De qué color es el caballo blanco de San Martin?", "Blanco"
   erb:portada
 end
 
 post '/' do
+  respuesta = params["respuesta"]
+  @resultado = @@Preg_resp.validar respuesta
   erb:portada
 end
