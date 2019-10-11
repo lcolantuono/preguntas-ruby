57243ba845b8dc0d43182cd22b7e0028c431afb3
vagrant <vagrant@kleer-csd-box>
2019-10-11 15:39:01 -0300

    Agrega preguntas

--- utils.rb
+++ utils.rb
@@ -7,5 +7,11 @@ def get_preg_resps
 	preg_multiple2.set_preg_multiple(pregunta, respuestas, 2)
 	preg_resp_multiple = PregMultiple.new
 	preg_resp_multiple.set_preg_multiple "¿De qué color es el caballo blanco de San Martin?", ["Blanco", "Rojo", "Azul"], 0
-	return [preg_multiple2, preg_resp_multiple]
+	preg_resp_multiple3 = PregMultiple.new
+	preg_resp_multiple3.set_preg_multiple "¿Qué haces al despertar que no puedes mientras duermes?", ["Comer", "Abrir los ojos", "Caminar"], 1
+	preg_resp_multiple4 = PregMultiple.new
+	preg_resp_multiple4.set_preg_multiple "¿Quién es el único que nunca pierde en un partido de fútbol?", ["Messi", "El diego", "El arbitro"], 2
+	preg_resp_multiple5 = PregMultiple.new
+	preg_resp_multiple5.set_preg_multiple "¿Qué es necesario para que un grupo de amigos con un solo paraguas no acabe empapado?", ["Que sean flacos", "Que no llueva", "Que el paraguas sea grande"], 1
+	return [preg_multiple2, preg_resp_multiple, preg_resp_multiple3,preg_resp_multiple4, preg_resp_multiple5]
 end
