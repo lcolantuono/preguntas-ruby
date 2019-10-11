48b23b7e768b89a02adb509334750ab0f923df48
vagrant <vagrant@kleer-csd-box>
2019-10-11 10:35:48 -0300

    Actualiza estilos de portada e integra clase Juego

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
 require 'sinatra'
 require './lib/PregResp.rb'
 require './lib/PregMultiple.rb'
-
+require "./lib/Juego.rb"
 
 get '/' do
   #@@Preg_resp = PregResp.new
   #@@Preg_resp.set_preg_resp "¿De qué color es el caballo blanco de San Martin?", "Blanco"
-  @@Preg_resp_multiple = PregMultiple.new
-	@@Preg_resp_multiple.set_preg_multiple "¿De qué color es el caballo blanco de San Martin?", ["Blanco", "Rojo", "Azul"], 0
-	@@respuestas = @@Preg_resp_multiple.get_respuestas
+  preg_resp_multiple = PregMultiple.new
+	preg_resp_multiple.set_preg_multiple "¿De qué color es el caballo blanco de San Martin?", ["Blanco", "Rojo", "Azul"], 0
+	@@juego = Juego.new [preg_resp_multiple]
   erb:portada
 end
 
 post '/' do
   respuesta = params["respuesta"].to_i
-  @resultado = @@Preg_resp_multiple.validar respuesta
+  @resultado = @@juego.get_siguiente_pregunta.validar respuesta
   erb:portada
 end
--- features/preguntas_multiples.feature
+++ features/preguntas_multiples.feature
@@ -4,14 +4,12 @@ Scenario: Validar seleccion correcta
 	Given I open the web app
 	When I select the right option
 		And I press "Responder"
-	Then I should see "Resultado"
-    And I should see "Correcto"
+	Then I should see "CORRECTO!"
 
 Scenario: Validar seleccion incorrecta
 	Given I open the web app
 	When I select an incorrect option
 		And I press "Responder"
-	Then I should see "Resultado"
-    And I should see "Incorrecto"
+	Then I should see "INCORRECTO!"
 
 
